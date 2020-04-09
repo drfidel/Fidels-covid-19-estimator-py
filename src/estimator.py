7bf3d08142fe56991156f1219f33ae1d7c229195
Akiyo Fidel <akiyofidel@gmail.com>
2020-04-09 19:25:47 +0300

    first commit

--- src/estimator.py
+++ src/estimator.py
@@ -1,2 +1,28 @@
 def estimator(data):
-  return data
+    {
+	region: {
+	name: "Africa",
+	avgAge: 19.7,
+	avgDailyIncomeInUSD: 5,
+	avgDailyIncomePopulation: 0.71
+	},
+	periodType: "days",
+	timeToElapse: 58,
+	reportedCases: 674,
+	population: 66622705,
+	totalHospitalBeds: 1380614
+	}
+
+{
+data: data, 
+impact: {
+	currentlyInfected = input_data['reportedCases']*10
+	infectionsByRequestedTime = input_data['currentlyInfected']*1024
+}, 
+severeImpact: {
+	currentlyInfected = input_data['reportedCases']*10
+	infectionsByRequestedTime = input_data['currentlyInfected']*1024
+} 
+}
+
+return data
