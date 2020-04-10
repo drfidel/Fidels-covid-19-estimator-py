26855d5ebee9d93b079a5d74938586d27efae67e
Akiyo Fidel <akiyofidel@gmail.com>
2020-04-10 15:09:13 +0300

    second attempt to remove err999 added dict

--- src/estimator.py
+++ src/estimator.py
@@ -1,28 +1,29 @@
 def estimator(data):
-    {
-	region: {
-	name: "Africa",
-	avgAge: 19.7,
-	avgDailyIncomeInUSD: 5,
-	avgDailyIncomePopulation: 0.71
-	},
-	periodType: "days",
-	timeToElapse: 58,
-	reportedCases: 674,
-	population: 66622705,
-	totalHospitalBeds: 1380614
-	}
+		input_data = {
+		"region": {
+		"name": "Africa",
+		"avgAge": 19.7,
+		"avgDailyIncomeInUSD": 5,
+		"avgDailyIncomePopulation": 0.71
+		},
+		"periodType": "days",
+		"timeToElapse": 58,
+		"reportedCases": 674,
+		"population": 66622705,
+		"totalHospitalBeds": 1380614
+		}
 
-{
-data: data, 
-impact: {
-	currentlyInfected = reportedCases*10
-	infectionsByRequestedTime = currentlyInfected*1024
-}, 
-severeImpact: {
-	currentlyInfected = reportedCases*10
-	infectionsByRequestedTime = currentlyInfected*1024
-} 
-}
+		put_data = {
+		"data":input_data, 
+		"impact": {
+			"currentlyInfected": input_data["reportedCases"]*10,
+			"infectionsByRequestedTime": input_data["currentlyInfected"]*1024
+			}, 
+	
+		"severeImpact": {
+			"currentlyInfected": input_data["reportedCases"]*10,
+			"infectionsByRequestedTime": input_data["currentlyInfected"]*1024
+			}
+		}
 
-return data
+		return output_data
